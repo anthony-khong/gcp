4e049b8d23003ccfd52d3cfb7399b7ee97d1d12f
Anthony Khong <anthony.kusumo.khong@gmail.com>
2018-11-10 10:20:39 +0700

    My notes on GCP

---
+++ notes.sh
@@ -0,0 +1,32 @@
+# List all available public images
+gcloud compute images list
+
+# Create Ubuntu instance for development
+gcloud compute instances create $NAME \
+    --image-family ubuntu-1804-lts \
+    --image-project ubuntu-os-cloud \
+    --metadata-from-file startup-script=startup.sh
+
+# Create COS instance for production
+gcloud compute instances create $NAME \
+    --image-family cos-stable \
+    --image-project gce-uefi-images
+
+# Start/Delete/Stop/Reset an instance
+gcloud compute instances $COMMAND $NAME
+
+# SSH when default project is set
+gcloud compute ssh $NAME
+
+# SSH to Cloud Shell
+gcloud alpha cloud-shell ssh
+
+# TODO:
+# - develop startup script 
+# - make sure you can start and stop
+# - test startup script
+# - find out how to scale up the instance
+# - create a new bitcoinmoney project
+
+# - Google storage
+# - Google Kubernetes Engine
